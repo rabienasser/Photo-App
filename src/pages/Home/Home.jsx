d63a9b2fe3b2904069dce19d7b0dc4786b54f6fc
Rabie <robienasser13@gmail.com>
2021-08-24 22:37:59 -0400

    added infinite scrolling and assets folder

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,35 +1,65 @@
-import React from 'react'
-import axios from 'axios'
-
-import Photo from '../../components/Photo/Photo'
+import React from "react";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Photo from "../../components/Photo/Photo";
 
 class Home extends React.Component {
-    state = {
-        photoData: null,
-    }
+   state = {
+      photoData: null,
+      page: 2,
+   };
 
-    loadRandomPhotos = async () => {
-        try {
-            const res = await axios(`https://api.unsplash.com/photos?order_by=latest&per_page=20&client_id=${process.env.REACT_APP_API_KEY}`)
-            const { data } = res
-            this.setState({ photoData: data })
-        } catch (err) {
-            console.log(err)
-        }
-    }
+   loadInitialPhotos = async () => {
+      try {
+         const res = await axios(
+            `https://api.unsplash.com/photos?order_by=latest&page=1&client_id=${process.env.REACT_APP_API_KEY}`
+         );
+         const { data } = res;
+         this.setState({ photoData: data });
+      } catch (err) {
+         console.log(err);
+      }
+   };
 
-    componentDidMount() {
-        this.loadRandomPhotos()
-    }
+   loadMorePhotos = async () => {
+      try {
+         const res = await axios(
+            `https://api.unsplash.com/photos?order_by=latest&page=${this.state.page}&client_id=${process.env.REACT_APP_API_KEY}`
+         );
+         const { data } = res;
+         this.setState({
+            photoData: this.state.photoData.concat(data),
+            page: this.state.page + 1,
+         });
+      } catch (err) {
+         console.log(err);
+      }
+   };
 
-    render() {
-        const { photoData } = this.state
-        return (
-            <div>
-                {photoData && photoData.map(photo => <Photo photo={photo} key={photo.id} />)}
-            </div>
-        )
-    }
+   componentDidMount() {
+      this.loadInitialPhotos();
+   }
+
+   render() {
+      const { photoData } = this.state;
+      return (
+         <div>
+            {photoData && (
+               <InfiniteScroll
+                  dataLength={photoData.length}
+                  next={() => this.loadMorePhotos()}
+                  hasMore={true}
+                  loader={<h4>Loading...</h4>}
+               >
+                  {photoData &&
+                     photoData.map((photo) => (
+                        <Photo photo={photo} key={photo.id} />
+                     ))}
+               </InfiniteScroll>
+            )}
+         </div>
+      );
+   }
 }
 
-export default Home
+export default Home;
